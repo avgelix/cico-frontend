4bc1a31b7a1f67db71b68807cca84cdceef477a7
Angela Petrone <apetrone@hawk.iit.edu>
2023-07-22 05:16:46 -0500

    Modify the activeloans page to include interest and total loaned/to pay info

--- activeloans.js
+++ activeloans.js
@@ -3,12 +3,39 @@ function showContent(option) {
   
     // Populate content based on the selected option
     switch (option) {
-      case 'lender':
-        activeLoans.innerHTML = "<h2>Option 1 Content</h2><p>This is the content for lender</p>";
-        break;
-      case 'lendee':
-        activeLoans.innerHTML = "<h2>Option 2 Content</h2><p>This is the content for lendee</p>";
-        break;
+        case 'lender':
+            activeLoans.innerHTML = `
+                <div class="activeLoansHeader">
+                    <div class="headerContent"><h2>Total Loaned:</h2> <p> $4000</p></div>
+                    <div class="headerContent"><h2>Interest Gained to Date:</h2> <p> $200</p></div>
+                </div>
+
+                <div class="loan">
+                    <h2>Loan 1</h2>
+                    <p>Loan details</p>
+                </div>
+                <div class="loan">
+                    <h2>Loan 2</h2>
+                    <p>Loan details</p>
+                </div>
+            `;
+            break;
+        case 'lendee':
+            activeLoans.innerHTML = `
+                <div class="activeLoansHeader">
+                    <div class="headerContent"> <h2>Total To Pay:</h2> <p> $6000</p></div>
+                    <div class="headerContent"> <h2>Interest Saved to Date:</h2> <p> $1000</p></div>
+                </div>
+                <div class="grid-item loan">
+                    <h2>Loan 1</h2>
+                    <p>Loan details</p>
+                </div>
+                <div class="grid-item loan">
+                    <h2>Loan 2</h2>
+                    <p>Loan details</p>
+                </div>
+            `;
+            break;
       default:
             contentDiv.innerHTML = "<h2>No Content Found</h2><p>Please select an option from the sidebar.</p>";
             break;
