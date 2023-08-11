77771e810c941228e5825a7df1e622b7a4433b11
Angela Petrone <apetrone@hawk.iit.edu>
2023-08-11 09:07:11 -0500

    Add error hadling logic for the loan creation form

--- src/main/createloan.js
+++ src/main/createloan.js
@@ -1,49 +1,95 @@
 document.addEventListener('DOMContentLoaded', () => {
   const createLoanForm = document.getElementById('loanTerms');
 
-  // Function to validate the form fields
-  function validateForm(event) {
-    const monthlyPaymentInput1 = document.getElementById('monthlyPayment').value;
-    const interestRateInput1 = document.getElementById('interestRate').value;
-    const balanceInput1 = document.getElementById('balance').value;
-    const startDateInput1 = document.getElementById('startDate').value;
-    const lenderInput1 = document.getElementById('lender').value;
-    const lendeeInput1 = document.getElementById('lendee').value;
+  const lenderNameError = document.getElementById('LenderNameError');
+  const lendeeNameError = document.getElementById('LendeeNameError');
+  const monthlyPaymentError = document.getElementById('monthlyPaymentError');
 
-    console.log("started")
+
+  // Function to validate the form fields
+  async function validateForm(event) {
+    document.getElementById('lender').addEventListener('input', () => {
+      lenderNameError.textContent = '';
+    });
+
+    document.getElementById('lendee').addEventListener('input', () => {
+      lendeeNameError.textContent = '';
+    });
+
+    document.getElementById('monthlyPayment').addEventListener('input', () => {
+      monthlyPaymentError.textContent = '';
+    });
 
     // Validate the input fields here
-    if (monthlyPaymentInput1 === '') {
+    if (document.getElementById('monthlyPayment').value === '') {
       alert('Monthly Payment is required.');
       console.log("no monthly payment")
       event.preventDefault(); // Prevent form submission
     }
 
-    if (interestRateInput1 === '') {
+    if (document.getElementById('interestRate').value === '') {
       alert('Interest Rate is required.');
-      event.preventDefault(); // Prevent form submission
+      return false; // Prevent form submission
     }
 
-    if (balanceInput1 === '') {
+    if (parseInt(document.getElementById('monthlyPayment').value) > parseInt(document.getElementById('balance').value)) {
+      monthlyPaymentError.textContent = "Monthly payment can't be bigger than loan amount";
+      return false; // Prevent form submission
+    }
+
+    if (document.getElementById('balance').value === '') {
       alert('Balance is required.');
-      event.preventDefault(); // Prevent form submission
+      return false;// Prevent form submission
     }
 
-    if (startDateInput1 === '') {
+    if (document.getElementById('startDate').value === '') {
       alert('Start Date is required.');
-      event.preventDefault(); // Prevent form submission
+      return false; // Prevent form submission
     }
 
-    if (lenderInput1 === '') {
+    if (document.getElementById('lender').value === '') {
       alert('Lender is required.');
-      event.preventDefault(); // Prevent form submission
+      event.preventDefault();// Prevent form submission
+    } else {
+      const lenderresponse = await fetch('http://localhost:3000/service/checkUser', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer ' + keycloak.token
+        },
+        body: JSON.stringify({ username: document.getElementById('lender').value })// Send the username as an object
+      });
+
+      const lender = await lenderresponse.json();
+      console.log(lender);
+
+      if (!lender.exists) {
+        lenderNameError.textContent = 'Not a valid user';
+        return false;
+      }
     }
 
-    if (lendeeInput1 === '') {
+    if (document.getElementById('lendee').value === '') {
       alert('Lendee is required.');
       event.preventDefault(); // Prevent form submission
-    }
+    } else {
+      const lendeeresponse = await fetch('http://localhost:3000/service/checkUser', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: 'Bearer ' + keycloak.token
+        },
+        body: JSON.stringify({ username: document.getElementById('lendee').value }) // Send the username as an object
+      });
 
+      const lendee = await lendeeresponse.json();
+      console.log(lendee);
+
+      if (!lendee.exists) {
+        lendeeNameError.textContent = 'Not a valid user';
+        return false;
+      }
+    }
 
     // If all validations pass
     // Get user input from the UI
@@ -56,14 +102,13 @@ document.addEventListener('DOMContentLoaded', () => {
     const lenderInput = document.getElementById('lender').value;
     const lendeeInput = document.getElementById('lendee').value;
 
-
     // Create the request data for the amorTable using user input
     const requestData = {
       monthlyPayment: monthlyPaymentInput,
       method: 'mortgage',
       apr: interestRateInput,
       balance: balanceInput,
-      loanTerm: -Math.log(1 - (((interestRateInput/100)/12) * balanceInput) / monthlyPaymentInput) / Math.log(1 + ((interestRateInput/100)/12)), /*Math.ceil(balanceInput / monthlyPaymentInput)*/
+      loanTerm: -Math.log(1 - (((interestRateInput / 100) / 12) * balanceInput) / monthlyPaymentInput) / Math.log(1 + ((interestRateInput / 100) / 12)), /*Math.ceil(balanceInput / monthlyPaymentInput)*/
       date: startDateInput
     };
 
@@ -72,11 +117,44 @@ document.addEventListener('DOMContentLoaded', () => {
     sessionStorage.setItem('lenderInput', lenderInput);
     sessionStorage.setItem('lendeeInput', lendeeInput);
 
-    window.location.href = 'loandetails.html';
-    event.preventDefault(); // Prevent form submission
+    return true;
   }
 
   // Add event listener to the form's submit event
-  createLoanForm.addEventListener('submit', validateForm);
+  /*createLoanForm.addEventListener('submit', (event) => {
+     Prevent the default form submission behavior
+    event.preventDefault();
+
+    console.log('prevented a disaster');
+
+    validateForm(event) 
+    .then(() => {
+        console.log('now..');
+        
+        if (validateForm(true)) {
+        // Submit the form programmatically after your function finishes processing
+        createLoanForm.submit();
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });*/
+
+  // Add event listener to the form's submit event
+  createLoanForm.addEventListener('submit', async (event) => {
+    // Prevent the default form submission behavior
+    event.preventDefault();
+
+    console.log('prevented a disaster');
+
+    const isValid = await validateForm(event);
+
+    if (isValid) {
+      console.log('now..');
+      createLoanForm.submit();
+    }
+
+    return false;
+  });
 
 });
